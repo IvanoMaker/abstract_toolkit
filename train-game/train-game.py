c26dedd599e13337fbc158d81c608a0e6b044405
IvanoMaker <emead098123@gmail.com>
2026-09-04 22:43:10 -0400

    added routes

--- train-game/train-game.py
+++ train-game/train-game.py
@@ -1,12 +1,33 @@
 import csv
 
-class Route:
-    def __init__(self, route_id, start_city, end_city, distance, color):
-        self.route_id = route_id
+STARTING_TRAIN_COUNT = 45
+TOTAL_COLOR_CARDS = {
+    'green': 12,
+    'pink': 12,
+    'blue': 12,
+    'white': 12,
+    'yellow': 12,
+    'orange': 12,
+    'black': 12,
+    'red': 12,
+    'multicolor': 14
+}
+
+class Path:
+    def __init__(self, path_id, start_city, end_city, distance, color, path_owner=None):
+        self.path_id = path_id
         self.start_city = start_city
         self.end_city = end_city
         self.distance = distance
         self.color = color
+        self.path_owner = path_owner
+
+class Route:
+    def __init__(self, point_a, point_b, distance, achieved_by=None):
+        self.point_a = point_a
+        self.point_b = point_b
+        self.distance = distance
+        self.achieved_by = achieved_by
 
 def read_csv(file_path):
     data = []
@@ -14,21 +35,32 @@ def read_csv(file_path):
         csv_reader = csv.reader(file)
         next(csv_reader)
         for row in csv_reader:
-            data.append(Route(*row))
+            data.append(Path(*row))
     return data
 
 if __name__ == "__main__":
-    file_path = 'train-game/routes.csv'
-    routes = read_csv(file_path)
+    connections_path = 'train-game/connections.csv'
+    routes_path = 'train-game/routes.csv'
+    paths = read_csv(connections_path)
+    routes = read_csv(routes_path)
 
-    cities = set()
-    colors = set()
+    cities = ['Atlanta', 'Boston', 'Calgary', 'Charleston', 'Chicago', 'Dallas', 
+              'Denver', 'Duluth', 'El Paso', 'Helena', 'Houston', 'Kansas City', 
+              'Las Vegas', 'Little Rock', 'Los Angeles', 'Miami', 'Montreal', 'Nashville', 
+              'New Orleans', 'New York', 'Oklahoma City', 'Omaha', 'Phoenix', 'Pittsburgh', 
+              'Portland', 'Raleigh', 'Saint Louis', 'Salt Lake City', 'San Francisco', 'Santa Fe', 
+              'Sault St Marie', 'Seattle', 'Toronto', 'Vancouver', 'Washington', 'Winnipeg']
 
-    for route in routes:
-        print(route.__dict__)
-        cities.add(route.start_city)
-        cities.add(route.end_city)
-        colors.add(route.color)
+    colors = ['na', 'green', 'pink', 'blue', 'white', 'yellow', 'orange', 'black', 'red']
 
-    print("Cities:", cities)
-    print("Colors:", colors)
+    dist_matrix = [[0 for _ in range(len(cities))] for _ in range(len(cities))]
+
+    for path in paths:
+        start_index = cities.index(path.start_city)
+        end_index = cities.index(path.end_city)
+        dist_matrix[start_index][end_index] = int(path.distance)
+        dist_matrix[end_index][start_index] = int(path.distance)
+            
+    print("Distance Matrix:")
+    for row in dist_matrix:
+        print(row)
