530e956c6f43dc6dfa39473932234ec488d8c184
IvanoMaker <emead098123@gmail.com>
2026-09-04 22:45:58 -0400

    fixed read_csv error

--- train-game/train-game.py
+++ train-game/train-game.py
@@ -13,6 +13,15 @@ TOTAL_COLOR_CARDS = {
     'multicolor': 14
 }
 
+CITIES = ['Atlanta', 'Boston', 'Calgary', 'Charleston', 'Chicago', 'Dallas', 
+              'Denver', 'Duluth', 'El Paso', 'Helena', 'Houston', 'Kansas City', 
+              'Las Vegas', 'Little Rock', 'Los Angeles', 'Miami', 'Montreal', 'Nashville', 
+              'New Orleans', 'New York', 'Oklahoma City', 'Omaha', 'Phoenix', 'Pittsburgh', 
+              'Portland', 'Raleigh', 'Saint Louis', 'Salt Lake City', 'San Francisco', 'Santa Fe', 
+              'Sault St Marie', 'Seattle', 'Toronto', 'Vancouver', 'Washington', 'Winnipeg']
+
+COLORS = ['na', 'green', 'pink', 'blue', 'white', 'yellow', 'orange', 'black', 'red']
+
 class Path:
     def __init__(self, path_id, start_city, end_city, distance, color, path_owner=None):
         self.path_id = path_id
@@ -38,26 +47,35 @@ def read_csv(file_path):
             data.append(Path(*row))
     return data
 
-if __name__ == "__main__":
+def setup():
     connections_path = 'train-game/connections.csv'
     routes_path = 'train-game/routes.csv'
-    paths = read_csv(connections_path)
-    routes = read_csv(routes_path)
 
-    cities = ['Atlanta', 'Boston', 'Calgary', 'Charleston', 'Chicago', 'Dallas', 
-              'Denver', 'Duluth', 'El Paso', 'Helena', 'Houston', 'Kansas City', 
-              'Las Vegas', 'Little Rock', 'Los Angeles', 'Miami', 'Montreal', 'Nashville', 
-              'New Orleans', 'New York', 'Oklahoma City', 'Omaha', 'Phoenix', 'Pittsburgh', 
-              'Portland', 'Raleigh', 'Saint Louis', 'Salt Lake City', 'San Francisco', 'Santa Fe', 
-              'Sault St Marie', 'Seattle', 'Toronto', 'Vancouver', 'Washington', 'Winnipeg']
+    connections_data = []
+    routes_data = []
 
-    colors = ['na', 'green', 'pink', 'blue', 'white', 'yellow', 'orange', 'black', 'red']
+    with open(connections_path, mode='r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            connections_data.append(Path(*row))
 
-    dist_matrix = [[0 for _ in range(len(cities))] for _ in range(len(cities))]
+    with open(routes_path, mode='r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            routes_data.append(Route(*row))
+
+    return connections_data, routes_data
+
+if __name__ == "__main__":
+
+    paths, routes = setup()
+    dist_matrix = [[0 for _ in range(len(CITIES))] for _ in range(len(CITIES))]
 
     for path in paths:
-        start_index = cities.index(path.start_city)
-        end_index = cities.index(path.end_city)
+        start_index = CITIES.index(path.start_city)
+        end_index = CITIES.index(path.end_city)
         dist_matrix[start_index][end_index] = int(path.distance)
         dist_matrix[end_index][start_index] = int(path.distance)
             
